bc3a05286745256ff6b922359890b8829b33f86c
Celestin <celestin.basura@gmail.com>
2014-05-23 20:18:17 +0200

    permission added

--- app/src/main/java/com/cele/diplomskibasura/app/SentronActivity.java
+++ app/src/main/java/com/cele/diplomskibasura/app/SentronActivity.java
@@ -3,9 +3,13 @@ package com.cele.diplomskibasura.app;
 import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.ghgande.j2mod.modbus.net.TCPConnectionHandler;
 import com.ghgande.j2mod.modbus.net.TCPMasterConnection;
@@ -20,62 +24,80 @@ public class SentronActivity extends Activity {
     TextView valueL1;
     TextView valueL2;
     TextView valueL3;
-    SharedPreferences sharedPreferences = getSharedPreferences(Constants.MY_PREFS, 0);
+    Button btnConnect;
+    Boolean isConnectedToSlave = false;
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sentron);
+        final SharedPreferences sharedPreferences1= getApplicationContext().getSharedPreferences(Constants.MY_PREFS, 0); // 0 - for private mode
+
 
         valueL1 = (TextView) findViewById(R.id.txt_sentron_l1);
         valueL2 = (TextView) findViewById(R.id.txt_sentron_l2);
         valueL3 = (TextView) findViewById(R.id.txt_sentron_l3);
+        btnConnect = (Button) findViewById(R.id.btn_sentron_connect);
 
+        btnConnect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(getApplicationContext(), "cliked", Toast.LENGTH_SHORT).show();
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        connectToDevice();
+                    }
+                });
+
+
+            }
+
+
+        });
 
 
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+
+
+   void connectToDevice(){
+
+        final String sentronIP = "192.168.0.1";//sharedPreferences.getString(Postavke.SENTRON_IP, Constants.DEFUALT_PAC_IP);
+        final int sentronPort = 502;//sharedPreferences.getInt(Postavke.SENTRON_PORT, Constants.DEFUALT_PAC_PORT);
+
+            try {
+
+
+                InetAddress address = InetAddress.getByName(sentronIP);
+                TCPMasterConnection conn = new TCPMasterConnection(address);
+                conn.setPort(sentronPort);
+                conn.setTimeout(1000);
+                Log.d("cele", "Connecting...");
+                conn.connect();
+
+                if(conn.isConnected()){
+                    Toast.makeText(getApplicationContext(), "Connected", Toast.LENGTH_SHORT).show();
+                    isConnectedToSlave = true;
+                }
+
+                 }catch (UnknownHostException e) {
+                Log.d("cele", "No host");
+
+                 } catch (Exception e) {
+                Log.d("cele", "failed to coinn");
+                    e.printStackTrace();
+                }
+
+
         }
-        return super.onOptionsItemSelected(item);
-    }
-
-
-    private void connectToDevice(){
-
-        String sentronIP = sharedPreferences.getString(Postavke.SENTRON_IP, Constants.DEFUALT_PAC_IP);
-        int sentronPort = sharedPreferences.getInt(Postavke.SENTRON_PORT, Constants.DEFUALT_PAC_PORT);
-
-
-        try {
-            InetAddress address = InetAddress.getByName(sentronIP);
-            TCPMasterConnection conn = new TCPMasterConnection(address);
-            conn.setPort(sentronPort);
-
-
-        } catch (UnknownHostException e) {
-
-
-
-        }
 
 
     }
 
 
-}
+
